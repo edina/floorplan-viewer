9b6a5cd3de71670f036ba4e93ea86676c74a6a76
Murray King <murray.king@ed.ac.uk>
2016-01-19 14:06:11 +0000

    Adding splash screen

--- app/src/main/java/uk/ac/edina/floorplan/FloorPlanViewActivity.java
+++ app/src/main/java/uk/ac/edina/floorplan/FloorPlanViewActivity.java
@@ -156,9 +156,7 @@ public class FloorPlanViewActivity extends FloorPlanBaseActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
